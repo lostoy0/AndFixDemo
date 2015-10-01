0833c4737c0dc0fc2b4b56ebbbe42161d7f8cc44
Raymond Lee <raymond007.lee@gmail.com>
2015-10-01 15:29:44 +0800

    no message

--- app/src/main/java/com/raymond/demo/andfixdemo/exception/CrashHandler.java
+++ app/src/main/java/com/raymond/demo/andfixdemo/exception/CrashHandler.java
@@ -26,8 +26,6 @@ import java.util.Map;
 /**
  * UncaughtException处理类,当程序发生Uncaught异常的时候,有该类来接管程序,并记录发送错误报告.
  *
- * @author fanlongli
- *
  */
 public class CrashHandler implements UncaughtExceptionHandler {
 
